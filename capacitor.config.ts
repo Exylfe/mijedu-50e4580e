757ef981c9054bf3e3895e3e900f8d6fbf4a591b
Exylfe <kennethmsaukila@gmail.com>
2026-03-30 17:57:31 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,13 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.mijedu.app',
   appName: 'Mijedu',
   webDir: 'dist',
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.mijedu.app',
-  appName: 'Mijedu',
-  webDir: 'dist',
 };
 
 export default config;
-  },
